756f003ff5b2435fc9360024e17bc3a42709b51f
Andrew Xue <aaxue@google.com>
2020-07-17 14:08:52 -0400

    support GKE resource detection (#24)
    add support for GKE resource detection

--- opentelemetry-exporter-cloud-trace/src/opentelemetry/exporter/cloud_trace/__init__.py
+++ opentelemetry-exporter-cloud-trace/src/opentelemetry/exporter/cloud_trace/__init__.py
@@ -303,10 +303,19 @@ def _strip_characters(ot_version):
 
 OT_RESOURCE_LABEL_TO_GCP = {
     "gce_instance": {
-        "cloud.account.id": "project_id",
         "host.id": "instance_id",
+        "cloud.account.id": "project_id",
         "cloud.zone": "zone",
-    }
+    },
+    "gke_container": {
+        "k8s.cluster.name": "cluster_name",
+        "k8s.namespace.name": "namespace_id",
+        "k8s.pod.name": "pod_id",
+        "host.id": "instance_id",
+        "container.name": "container_name",
+        "cloud.account.id": "project_id",
+        "cloud.zone": "zone",
+    },
 }
 
 
