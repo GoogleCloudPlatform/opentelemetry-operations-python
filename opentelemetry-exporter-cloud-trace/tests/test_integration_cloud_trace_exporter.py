40ebd94fa71c7187f7c40f84ff96f26f5235d98e
the-ericwang35 <the.ericwang35@gmail.com>
2020-07-24 14:40:48 +0000

    Address comments

--- opentelemetry-exporter-cloud-trace/tests/test_integration_cloud_trace_exporter.py
+++ opentelemetry-exporter-cloud-trace/tests/test_integration_cloud_trace_exporter.py
@@ -15,7 +15,6 @@
 import socket
 import subprocess
 import unittest
-from time import sleep
 
 import grpc
 from google.cloud.trace_v2 import TraceServiceClient
@@ -24,10 +23,13 @@ from opentelemetry.exporter.cloud_trace import CloudTraceSpanExporter
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace.export import Span, SpanExportResult
 from opentelemetry.trace import SpanContext, SpanKind
+from opentelemetry.util import time_ns
 
 
 class BaseExporterIntegrationTest(unittest.TestCase):
     def setUp(self):
+        self.project_id = "TEST-PROJECT"
+
         # Find a free port to spin up our server at.
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.bind(("localhost", 0))
@@ -35,8 +37,12 @@ class BaseExporterIntegrationTest(unittest.TestCase):
         sock.close()
 
         # Start the mock server.
-        args = "mock_server-x64-linux -address=" + self.address
-        self.mock_server_process = subprocess.Popen(args, shell=True)
+        args = ["mock_server-x64-linux", "-address", self.address]
+        self.mock_server_process = subprocess.Popen(
+            args, stderr=subprocess.PIPE
+        )
+        # Block until the mock server starts (it will output the address after starting).
+        self.mock_server_process.stderr.readline()
 
     def tearDown(self):
         self.mock_server_process.kill()
@@ -44,7 +50,6 @@ class BaseExporterIntegrationTest(unittest.TestCase):
 
 class TestCloudTraceSpanExporter(BaseExporterIntegrationTest):
     def test_export(self):
-        project_id = "TEST-PROJECT"
         trace_id = "6e0c63257de34c92bf9efcd03927272e"
         span_id = "95bb5edabd45950f"
 
@@ -70,19 +75,20 @@ class TestCloudTraceSpanExporter(BaseExporterIntegrationTest):
             resource=resource_info,
             attributes={"attr_key": "attr_value"},
         )
-        span.start()
-        sleep(0.1)
-        span.end()
+
+        # pylint: disable=protected-access
+        span._start_time = int(time_ns() - (60 * 1e9))
+        span._end_time = time_ns()
         span_data = [span]
 
-        # Setup the exporter.
+        # Setup the trace exporter.
         channel = grpc.insecure_channel(self.address)
         transport = trace_service_grpc_transport.TraceServiceGrpcTransport(
             channel=channel
         )
         client = TraceServiceClient(transport=transport)
-        exporter = CloudTraceSpanExporter(project_id, client=client)
+        trace_exporter = CloudTraceSpanExporter(self.project_id, client=client)
 
         # Export the spans and verify the results.
-        result = exporter.export(span_data)
+        result = trace_exporter.export(span_data)
         self.assertEqual(result, SpanExportResult.SUCCESS)
