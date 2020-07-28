fa12fb9eca94f3e6ab3e38e3b3863c1c8b12f291
Andrew Xue <aaxue@google.com>
2020-07-28 16:06:21 -0400

    final update

--- opentelemetry-exporter-cloud-monitoring/tests/test_cloud_monitoring.py
+++ opentelemetry-exporter-cloud-monitoring/tests/test_cloud_monitoring.py
@@ -70,15 +70,6 @@ class MockMetric:
 
 
 class TestCloudMonitoringMetricsExporter(unittest.TestCase):
-    def setUp(self):
-        self.client_patcher = mock.patch(
-            "opentelemetry.exporter.cloud_monitoring.MetricServiceClient"
-        )
-        self.client_patcher.start()
-
-    def tearDown(self) -> None:
-        self.client_patcher.stop()
-
     @classmethod
     def setUpClass(cls):
         cls.project_id = "PROJECT"
