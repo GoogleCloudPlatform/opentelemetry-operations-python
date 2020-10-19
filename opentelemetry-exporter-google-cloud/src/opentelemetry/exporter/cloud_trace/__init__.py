9bd06d44c96bf265ab5143ecc877263e7575c734
Aaron Abbott <aaronabbott@google.com>
2020-10-19 17:55:49 +0000

    add dependency version specifiers and update docs for span processors

--- opentelemetry-exporter-google-cloud/src/opentelemetry/exporter/cloud_trace/__init__.py
+++ opentelemetry-exporter-google-cloud/src/opentelemetry/exporter/cloud_trace/__init__.py
@@ -23,18 +23,26 @@ Usage
     from opentelemetry import trace
     from opentelemetry.exporter.cloud_trace import CloudTraceSpanExporter
     from opentelemetry.sdk.trace import TracerProvider
+
+    # For debugging
     from opentelemetry.sdk.trace.export import SimpleExportSpanProcessor
+    # Otherwise
+    from opentelemetry.sdk.trace.export import BatchExportSpanProcessor
 
     trace.set_tracer_provider(TracerProvider())
 
     cloud_trace_exporter = CloudTraceSpanExporter()
     trace.get_tracer_provider().add_span_processor(
-        SimpleExportSpanProcessor(cloud_trace_exporter)
+        BatchExportSpanProcessor(cloud_trace_exporter)
     )
     tracer = trace.get_tracer(__name__)
     with tracer.start_as_current_span("foo"):
         print("Hello world!")
 
+
+When not debugging, make sure to use
+:class:`opentelemetry.sdk.trace.export.BatchExportSpanProcessor` with the
+default parameters for performance reasons.
 
 API
 ---
