e9c3f1065ececb2e94af3dc7ad8cbf241665cfd4
Aaron Abbott <aaronabbott@google.com>
2021-04-08 21:20:07 +0000

    flask e2e example with region tags

---
+++ docs/examples/flask_e2e/client.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# Copyright 2021 The OpenTelemetry Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START opentelemetry_flask_client_import]
+
+import requests
+from opentelemetry import trace
+from opentelemetry.exporter.cloud_trace import CloudTraceSpanExporter
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.propagate import set_global_textmap
+from opentelemetry.propagators.cloud_trace_propagator import (
+    CloudTraceFormatPropagator,
+)
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+# [END opentelemetry_flask_client_import]
+
+# [START opentelemetry_flask_client_setup_propagator]
+
+set_global_textmap(CloudTraceFormatPropagator())
+
+# [END opentelemetry_flask_client_setup_propagator]
+
+# [START opentelemetry_flask_client_setup_exporter]
+
+tracer_provider = TracerProvider()
+cloud_trace_exporter = CloudTraceSpanExporter()
+tracer_provider.add_span_processor(
+    # BatchSpanProcessor buffers spans and sends them in batches in a
+    # background thread. The default parameters are sensible, but can be
+    # tweaked to optimize your performance
+    BatchSpanProcessor(cloud_trace_exporter)
+)
+trace.set_tracer_provider(tracer_provider)
+
+tracer = trace.get_tracer(__name__)
+
+# [END opentelemetry_flask_client_setup_exporter]
+
+# [START opentelemetry_flask_client_instrument]
+
+RequestsInstrumentor().instrument()
+
+res = requests.get("http://localhost:6000")
+print(res.text)
+
+# [END opentelemetry_flask_client_instrument]
