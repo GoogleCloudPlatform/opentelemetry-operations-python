e73bc33b73abffea9dabbab663a7f8ef235bea16
Aaron Abbott <aaronabbott@google.com>
2022-11-05 02:55:41 +0000

    Add snapshot test setup code and basic tests

---
+++ opentelemetry-exporter-gcp-monitoring/tests/fixtures/snapshot_gcmcalls.py
@@ -0,0 +1,77 @@
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import Optional, cast
+
+import google.protobuf.message
+import proto
+import pytest
+from fixtures.gcmfake import GcmCalls
+from google.protobuf import json_format
+from syrupy.extensions.json import JSONSnapshotExtension
+from syrupy.matchers import path_type
+from syrupy.types import (
+    PropertyFilter,
+    PropertyMatcher,
+    SerializableData,
+    SerializedData,
+)
+
+
+# pylint: disable=too-many-ancestors
+class GcmCallsSnapshotExtension(JSONSnapshotExtension):
+    """syrupy extension to serialize GcmCalls.
+
+    Serializes the protobufs for each method call into JSON for storing as a snapshot.
+    """
+
+    def serialize(
+        self,
+        data: SerializableData,
+        *,
+        exclude: Optional[PropertyFilter] = None,
+        matcher: Optional[PropertyMatcher] = None,
+    ) -> SerializedData:
+        calls = cast(GcmCalls, data)
+        json = {}
+        for method, requests in calls.items():
+            dict_requests = []
+            for request in requests:
+                if isinstance(request, proto.message.Message):
+                    request = type(request).pb(request)
+                elif isinstance(request, google.protobuf.message.Message):
+                    pass
+                else:
+                    raise ValueError(
+                        f"Excepted a proto-plus or protobuf message, got {type(request)}"
+                    )
+                dict_requests.append(json_format.MessageToDict(request))
+            json[method] = dict_requests
+
+        return super().serialize(json, exclude=exclude, matcher=matcher)
+
+
+@pytest.fixture(name="snapshot_gcmcalls")
+def fixture_snapshot_gcmcalls(snapshot):
+    """Fixture for snapshot testing of GcmCalls
+
+    TimeInterval.start_time and TimeInterval.end_time timestamps are "redacted" since they are
+    dynamic depending on when the test is run.
+    """
+    return snapshot.use_extension(GcmCallsSnapshotExtension)(
+        matcher=path_type(
+            {r".*\.interval\.(start|end)Time": (str,)},
+            regex=True,
+        )
+    )
--- opentelemetry-exporter-gcp-monitoring/tests/test_cloud_monitoring.py
+++ opentelemetry-exporter-gcp-monitoring/tests/test_cloud_monitoring.py
@@ -13,15 +13,71 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""
+Some tests in this file use [syrupy](https://github.com/tophat/syrupy) for snapshot testing aka
+golden testing. The GCM API calls are captured with a gRPC fake and compared to the existing
+snapshot file in the __snapshots__ directory.
+
+If an expected behavior change is made to the exporter causing these tests to fail, regenerate
+the snapshots by running tox to pass the --snapshot-update flag to pytest:
+
+```sh
+tox -e py310-ci-test-cloudmonitoring -- --snapshot-update
+```
+
+Be sure to review the changes.
+"""
+
+from typing import Iterable
+
+import pytest
+from fixtures.gcmfake import GcmFake
 from google.auth.credentials import AnonymousCredentials
 from google.cloud.monitoring_v3 import MetricServiceClient
 from opentelemetry.exporter.cloud_monitoring import (
     CloudMonitoringMetricsExporter,
 )
+from opentelemetry.sdk.metrics import MeterProvider
+from opentelemetry.sdk.metrics.export import PeriodicExportingMetricReader
+from opentelemetry.util.types import Attributes
 
 PROJECT_ID = "fakeproject"
+LABELS: Attributes = {
+    "string": "string",
+    "int": 123,
+    "float": 123.4,
+}
+
+
+@pytest.fixture(name="meter_provider")
+def fixture_meter_provider(gcmfake: GcmFake) -> Iterable[MeterProvider]:
+    mp = MeterProvider(
+        metric_readers=[
+            PeriodicExportingMetricReader(
+                CloudMonitoringMetricsExporter(
+                    project_id=PROJECT_ID, client=gcmfake.client
+                )
+            )
+        ],
+        shutdown_on_exit=False,
+    )
+    yield mp
+    mp.shutdown()
 
 
 def test_create_monitoring_exporter() -> None:
     client = MetricServiceClient(credentials=AnonymousCredentials())
     CloudMonitoringMetricsExporter(project_id=PROJECT_ID, client=client)
+
+
+def test_counter(
+    meter_provider: MeterProvider,
+    gcmfake: GcmFake,
+    snapshot_gcmcalls,
+) -> None:
+    counter = meter_provider.get_meter(__name__).create_counter(
+        "mycounter", description="foo", unit="{myunit}"
+    )
+    counter.add(123, LABELS)
+    meter_provider.force_flush()
+    assert gcmfake.get_calls() == snapshot_gcmcalls
