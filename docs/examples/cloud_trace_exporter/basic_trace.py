bc55ec39591b2e8e369f8f8f44570afa22189f25
Aaron Abbott <aaronabbott@google.com>
2024-07-18 14:31:03 -0400

    Update a few examples broken or misleading examples (#342)
    - Remove any usage of SimpleSpanProcessor
    - Fix import paths for resource detector which were missed earlier
    - Fix use of pre-1.0 metrics SDK

--- docs/examples/cloud_trace_exporter/basic_trace.py
+++ docs/examples/cloud_trace_exporter/basic_trace.py
@@ -15,13 +15,13 @@
 from opentelemetry import trace
 from opentelemetry.exporter.cloud_trace import CloudTraceSpanExporter
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import SimpleSpanProcessor
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
 trace.set_tracer_provider(TracerProvider())
 
 cloud_trace_exporter = CloudTraceSpanExporter()
 trace.get_tracer_provider().add_span_processor(
-    SimpleSpanProcessor(cloud_trace_exporter)
+    BatchSpanProcessor(cloud_trace_exporter)
 )
 tracer = trace.get_tracer(__name__)
 with tracer.start_as_current_span("foo"):
