4124b9b25ffc380acf5b3f3874fed97e7af6cb32
Michael Aaron Safyan <michaelsafyan@google.com>
2025-02-13 16:29:27 -0500

    Implement metrics export.

--- opentelemetry-configurator-gcp/src/opentelemetry/configurator/gcp/metrics.py
+++ opentelemetry-configurator-gcp/src/opentelemetry/configurator/gcp/metrics.py
@@ -1,5 +1,15 @@
+from opentelemetry import metrics as otel_metrics
+from opentelemetry.sdk import metrics as otel_metrics_sdk
+from opentelemetry.sdk.metrics import export as otel_metrics_sdk_export
+from opentelemetry.exporter import cloud_monitoring as otel_cloud_monitoring
 
 
 def configure_metrics_exporter(resource=None):
-    # TODO:...
-    pass
+    provider = otel_metrics_sdk.MeterProvider(
+        metric_readers=[
+            otel_metrics_sdk_export.PeriodicExportingMetricReader(
+                otel_cloud_monitoring.CloudMonitoringMetricsExporter()
+            ),
+        ],
+        resource=resource)
+    otel_metrics.set_meter_provider(provider)
