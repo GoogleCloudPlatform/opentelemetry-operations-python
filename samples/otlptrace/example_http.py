614867987ac3ca0aa7d70f00eb25808d6ee978cf
Dylan Russell <dylanrussell@google.com>
2025-04-02 17:15:11 +0000

    Fix formatting

--- samples/otlptrace/example_http.py
+++ samples/otlptrace/example_http.py
@@ -25,14 +25,15 @@ from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
 credentials, _ = google.auth.default()
-resource = Resource.create(attributes={SERVICE_NAME: "otlp-gcp-http-sample"})
-
-trace_provider = TracerProvider(resource=resource)
+trace_provider = TracerProvider(
+    resource=Resource.create(attributes={SERVICE_NAME: "otlp-gcp-http-sample"})
+)
 processor = BatchSpanProcessor(
     OTLPSpanExporter(
-       session=AuthorizedSession(credentials),
-       endpoint="https://telemetry.googleapis.com:443/v1/traces"
-    ))
+        session=AuthorizedSession(credentials),
+        endpoint="https://telemetry.googleapis.com:443/v1/traces",
+    )
+)
 trace_provider.add_span_processor(processor)
 trace.set_tracer_provider(trace_provider)
 tracer = trace.get_tracer("my.tracer.name")
